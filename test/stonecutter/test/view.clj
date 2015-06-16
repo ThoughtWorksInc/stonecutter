33a845625062e974b5d96d938ca639b0a4a8b9c6
Chris Cheshire <chris.cheshire@gmail.com>
2015-06-16 15:13:26 +0100

    Update 2015-06-16T14:13:22.645Z

--- test/stonecutter/test/view.clj
+++
@@ -1,9 +0,0 @@
-(ns stonecutter.test.view
-  (:require [midje.sweet :refer :all]
-            [net.cgrand.enlive-html :as html]
-            [stonecutter.view :refer [registration-form]]
-            ))
-
-(fact "registration-form should return some html"
-      (let [page (-> (registration-form) html/html-snippet)]
-        (-> page (html/select [:form])) =not=> empty?))
