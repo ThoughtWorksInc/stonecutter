0e489645a11595d3c56f770e655fa4ec07a6967b
John Cowie <john.a.cowie@gmail.com>
2015-06-22 17:51:41 +0100

    JC |17| Implemented fetch in mongo store

--- src/stonecutter/mongo.clj
+++ src/stonecutter/mongo.clj
@@ -6,7 +6,10 @@
 
 (defrecord MongoStore [mongo-db]
   s/Store
-  (fetch [this t])
+  (fetch [this t]
+    (->
+      (mc/find-one-as-map mongo-db user-collection {:login t})
+      (dissoc :_id)))
   (revoke! [this k])
   (store! [this k item]
     (->
--- test/stonecutter/integration/mongo.clj
+++ test/stonecutter/integration/mongo.clj
@@ -7,18 +7,36 @@
     [stonecutter.mongo :refer [new-mongo-store]]))
 
 (def test-db "stonecutter-test")
-(def conn (m/connect))
-(def db (m/get-db conn test-db))
+(def coll "users")
 
+(def db (atom nil))
+(def conn (atom nil))
+
+(defn connect []
+  (swap! conn (constantly (m/connect)))
+  (swap! db (constantly (m/get-db @conn test-db))))
+
+(defn clear-data []
+  (m/drop-db @conn test-db))
+
+(defn disconnect []
+  (m/disconnect @conn))
+
+(background
+  (before :facts (do (connect) (clear-data))
+   :after (do (clear-data) (disconnect))))
 
 
 (facts "about storing"
-       (let [store (new-mongo-store db)]
-         (c/find-maps db "users") => empty?
+       (let [store (new-mongo-store @db)]
+         (c/find-maps @db coll) => empty?
          (s/store! store "userA" {:login "userA" :password "password"}) => {:login "userA" :password "password"}
-         (count (c/find-maps db "users")) => 1
-         (c/find-one-as-map db "users" {:login "userA"}) => (contains {:login "userA" :password "password"})))
+         (count (c/find-maps @db coll)) => 1
+         (c/find-one-as-map @db coll {:login "userA"}) => (contains {:login "userA" :password "password"})))
 
-(m/drop-db conn test-db)
-(m/disconnect conn)
+(facts "about fetching"
+       (let [store (new-mongo-store @db)]
+         (c/insert @db coll {:login "userA" :password "passwordA"})
+         (s/fetch store "userA") => {:login "userA" :password "passwordA"}))
+
 
