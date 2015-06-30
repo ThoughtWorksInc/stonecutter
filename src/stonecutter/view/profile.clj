f59103336ce79fdade340f3558016e2999732078
Andrei Biasprozvanny <abiaspro@thoughtworks.com>
2015-06-30 17:44:10 +0100

    CW + AB | 0 | remove work in progress elements from profile view

--- src/stonecutter/view/profile.clj
+++ src/stonecutter/view/profile.clj
@@ -13,12 +13,17 @@
   (html/at enlive-m 
            [:.clj--sign-out__link] (html/set-attr :href (r/path :sign-out))))
 
+(defn remove-work-in-progress [enlive-m]
+  (html/at enlive-m
+           [:.clj-wip] nil))
+
 (defn pri [v] (prn v) v)
 
 (defn profile [request]
   (let [context (:context request)
         translator (:translator context)]
     (->> (vh/load-template "public/profile.html")
+         remove-work-in-progress
          (add-username request)
          add-sign-out-link
          (t/translate translator)
