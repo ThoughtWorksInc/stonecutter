70ce368bd017fd47091e4ca71658e4a667990575
Rob Speller <rspeller@thoughtworks.com>
2015-07-10 17:49:15 +0100

    RS | Refactoring requires

--- src/stonecutter/db/storage.clj
+++ src/stonecutter/db/storage.clj
@@ -1,51 +1,51 @@
 (ns stonecutter.db.storage
-  (:require [clauth.user :as user-store]
-            [clauth.token :as token-store]
-            [clauth.client :as client-store]
-            [clauth.auth-code :as auth-code-store]
-            [clauth.store :as store]
+  (:require [clauth.user :as cl-user]
+            [clauth.token :as cl-token]
+            [clauth.client :as cl-client]
+            [clauth.auth-code :as cl-auth-code]
+            [clauth.store :as cl-store]
             [clojure.string :as s]
             [clojure.tools.logging :as log]
             [stonecutter.db.mongo :as m])
   (:import (java.util UUID)))
 
 (defn setup-mongo-stores! [db]
-  (swap! user-store/user-store (constantly (m/create-mongo-user-store db)))
-  (swap! token-store/token-store (constantly (m/create-token-store db)))
-  (swap! auth-code-store/auth-code-store (constantly (m/create-auth-code-store db)))
-  (swap! client-store/client-store (constantly (m/create-client-store db))))
+  (swap! cl-user/user-store (constantly (m/create-mongo-user-store db)))
+  (swap! cl-token/token-store (constantly (m/create-token-store db)))
+  (swap! cl-auth-code/auth-code-store (constantly (m/create-auth-code-store db)))
+  (swap! cl-client/client-store (constantly (m/create-client-store db))))
 
 (defn reset-mongo-stores! [mongo-uri]
   (log/info "Resetting mongo stores")
   (let [db (m/get-mongo-db mongo-uri)]
-    (store/reset-store! @user-store/user-store)
-    (store/reset-store! @token-store/token-store)
-    (store/reset-store! @auth-code-store/auth-code-store)
-    (store/reset-store! @client-store/client-store)
-    (swap! user-store/user-store (constantly (m/create-mongo-user-store db)))
-    (swap! token-store/token-store (constantly (m/create-token-store db)))
-    (swap! auth-code-store/auth-code-store (constantly (m/create-auth-code-store db)))
-    (swap! client-store/client-store (constantly (m/create-client-store db)))))
+    (cl-store/reset-store! @cl-user/user-store)
+    (cl-store/reset-store! @cl-token/token-store)
+    (cl-store/reset-store! @cl-auth-code/auth-code-store)
+    (cl-store/reset-store! @cl-client/client-store)
+    (swap! cl-user/user-store (constantly (m/create-mongo-user-store db)))
+    (swap! cl-token/token-store (constantly (m/create-token-store db)))
+    (swap! cl-auth-code/auth-code-store (constantly (m/create-auth-code-store db)))
+    (swap! cl-client/client-store (constantly (m/create-client-store db)))))
 
 (defn setup-in-memory-stores! []
-  (swap! user-store/user-store (constantly (store/create-memory-store)))
-  (swap! token-store/token-store (constantly (store/create-memory-store)))
-  (swap! auth-code-store/auth-code-store (constantly (store/create-memory-store)))
-  (swap! client-store/client-store (constantly (store/create-memory-store))))
+  (swap! cl-user/user-store (constantly (cl-store/create-memory-store)))
+  (swap! cl-token/token-store (constantly (cl-store/create-memory-store)))
+  (swap! cl-auth-code/auth-code-store (constantly (cl-store/create-memory-store)))
+  (swap! cl-client/client-store (constantly (cl-store/create-memory-store))))
 
 (defn reset-in-memory-stores! []
-  (reset! user-store/user-store (store/create-memory-store))
-  (reset! token-store/token-store  (store/create-memory-store))
-  (reset! auth-code-store/auth-code-store  (store/create-memory-store))
-  (reset! client-store/client-store (store/create-memory-store)))
+  (reset! cl-user/user-store (cl-store/create-memory-store))
+  (reset! cl-token/token-store (cl-store/create-memory-store))
+  (reset! cl-auth-code/auth-code-store (cl-store/create-memory-store))
+  (reset! cl-client/client-store (cl-store/create-memory-store)))
 
 (defn is-duplicate-user? [email]
-  (not (nil? (user-store/fetch-user (s/lower-case email)))))
+  (not (nil? (cl-user/fetch-user (s/lower-case email)))))
 
 (defn create-user [id-gen email password]
   (let [lower-email (s/lower-case email)]
     (->
-      (user-store/new-user lower-email password)
+      (cl-user/new-user lower-email password)
       (assoc :uid (id-gen)))))
 
 (defn uuid []
@@ -53,15 +53,15 @@
 
 (defn store-user! [email password]
   (-> (create-user uuid email password)
-      user-store/store-user
+      cl-user/store-user
       (dissoc :password)))
 
 (defn delete-user! [email]
-  (store/revoke! @user-store/user-store email))
+  (cl-store/revoke! @cl-user/user-store email))
 
 (defn authenticate-and-retrieve-user [email password]
-  (-> (user-store/authenticate-user email password)
+  (-> (cl-user/authenticate-user email password)
       (dissoc :password)))
 
 (defn retrieve-user-with-auth-code [code]
-  (-> (auth-code-store/fetch-auth-code code) :subject))
+  (-> (cl-auth-code/fetch-auth-code code) :subject))
