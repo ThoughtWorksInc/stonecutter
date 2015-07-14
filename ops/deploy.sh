dbe99c4affd4762b03287c8d58f6c1fb7897800c
John Cowie <jcowie@thoughtworks.com>
2015-07-14 11:11:58 +0100

    JC | Add environment variable file for local vagrant deploy

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -5,5 +5,5 @@
 ssh $REMOTE_USER@$SERVER_IP << EOF
   sudo docker stop stonecutter || echo "Failed to stop stonecutter container"
   sudo docker rm stonecutter || echo "Failed to remove stonecutter container"
-  sudo docker run -d -v /var/stonecutter/target:/var/stonecutter -p 5000:3000 --name stonecutter --link mongo:mongo -e "HOST=0.0.0.0" java:8 bash -c 'java -jar /var/stonecutter/*standalone.jar'
+  sudo docker run -d -v /var/stonecutter/target:/var/stonecutter -p 5000:3000 --name stonecutter --link mongo:mongo --env-file=/var/stonecutter/config/stonecutter.env java:8 bash -c 'java -jar /var/stonecutter/*standalone.jar'
 EOF
--- ops/deploy_snap.sh
+++ ops/deploy_snap.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/stonecutter/target"
-scp target/*-standalone.jar $REMOTE_USER@$SERVER_IP:/var/helsinki/dist/stonecutter.jar
+ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/stonecutter/config"
+scp target/*-standalone.jar $REMOTE_USER@$SERVER_IP:/var/stonecutter/target/stonecutter-standalone.jar
+scp stonecutter.env $REMOTE_USER@$SERVER_IP:/var/stonecutter/config/stonecutter.env
 ./deploy.sh
 # Steps
  # scp app onto digital ocean
