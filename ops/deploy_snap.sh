760458eff74cbde1381a1a3a12a850e8e8713fd6
John Cowie <jcowie@thoughtworks.com>
2015-07-14 18:17:42 +0100

    JC | Extended deploy script and tested locally

--- ops/deploy_snap.sh
+++ ops/deploy_snap.sh
@@ -2,11 +2,11 @@
 
 ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/stonecutter/target"
 ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/stonecutter/config"
-scp ../target/*-standalone.jar $REMOTE_USER@$SERVER_IP:/var/stonecutter/target/stonecutter-standalone.jar
-scp ../stonecutter.env $REMOTE_USER@$SERVER_IP:/var/stonecutter/config/stonecutter.env
-scp ../clients.yml $REMOTE_USER@$SERVER_IP:/var/stonecutter/config/clients.yml
-./deploy.sh
-# Steps
- # scp app onto digital ocean
- # scp config file onto digital ocean
- # run deployment script
+scp target/*-standalone.jar $REMOTE_USER@$SERVER_IP:/var/stonecutter/target/stonecutter-standalone.jar
+scp stonecutter.env $REMOTE_USER@$SERVER_IP:/var/stonecutter/config/stonecutter.env
+scp clients.yml $REMOTE_USER@$SERVER_IP:/var/stonecutter/config/clients.yml
+ssh $REMOTE_USER@$SERVER_IP <<EOF
+  sudo docker stop stonecutter || echo 'Failed to stop stonecutter container'
+  sudo docker rm stonecutter || echo 'Failed to remove stonecutter container'
+  sudo docker run -d -v /var/stonecutter/target:/var/stonecutter -v /var/stonecutter/config/clients.yml:/var/config/clients.yml -p 5000:3000 --name stonecutter --link mongo:mongo "--env-file=/var/stonecutter/config/stonecutter.env" java:8 bash -c 'java -jar /var/stonecutter/*standalone.jar'
+EOF
