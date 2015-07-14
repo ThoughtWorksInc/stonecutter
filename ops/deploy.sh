39b6699a41f35d2d194d8eb4b067094b0a009a10
John Cowie <jcowie@thoughtworks.com>
2015-07-14 16:01:43 +0100

    JC | Use double quotes in deploy script to escape environment variables

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ssh $REMOTE_USER@$SERVER_IP << EOF
-  sudo docker stop stonecutter || echo "Failed to stop stonecutter container"
-  sudo docker rm stonecutter || echo "Failed to remove stonecutter container"
-  sudo docker run -d -v /var/stonecutter/target:/var/stonecutter -v $CLIENT_CREDENTIALS_FILE_PATH:/var/config/clients.yml -p 5000:3000 --name stonecutter --link mongo:mongo --env-file=$ENV_FILE_PATH java:8 bash -c 'java -jar /var/stonecutter/*standalone.jar'
+ssh $REMOTE_USER@$SERVER_IP <<EOF
+  sudo docker stop stonecutter || echo 'Failed to stop stonecutter container'
+  sudo docker rm stonecutter || echo 'Failed to remove stonecutter container'
+  sudo docker run -d -v /var/stonecutter/target:/var/stonecutter -v "$CLIENT_CREDENTIALS_FILE_PATH:/var/config/clients.yml" -p 5000:3000 --name stonecutter --link mongo:mongo "--env-file=$ENV_FILE_PATH" java:8 bash -c 'java -jar /var/stonecutter/*standalone.jar'
 EOF
--- ops/deploy_vagrant.sh
+++ ops/deploy_vagrant.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-REMOTE_USER=vagrant SERVER_IP=192.168.50.61 CLIENT_CREDENTIALS_FILE_PATH=/var/stonecutter/config/dummy-clients.yml ENV_FILE_PATH=/var/stonecutter/config/stonecutter.env ./deploy.sh
+REMOTE_USER=vagrant \
+SERVER_IP=192.168.50.61 \
+CLIENT_CREDENTIALS_FILE_PATH=/var/stonecutter/config/dummy-clients.yml \
+ENV_FILE_PATH=/var/stonecutter/config/stonecutter.env \
+./deploy.sh
