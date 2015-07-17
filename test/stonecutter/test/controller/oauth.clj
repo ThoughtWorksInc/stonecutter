9803b16ccc5af4d2aa258c5a36598352c7a25010
Duncan Mortimer <dmortime@thoughtworks.com>
2015-07-17 13:59:25 +0100

    DM + RS | 6 | Update oauth controller tests to use create-request function and routes/path

--- test/stonecutter/test/controller/oauth.clj
+++ test/stonecutter/test/controller/oauth.clj
@@ -14,7 +14,7 @@
   (:import (org.apache.commons.codec.binary Base64)))
 
 (defn create-request [method url params]
-  (-> (r/request method url)
+  (-> (r/request method url params)
       (assoc :params params)
       (assoc-in [:context :translator] {})))
 
@@ -27,8 +27,10 @@
 (facts "about authorisation end-point"
        (fact "request with client-id, response_type and redirect_uri returns redirect to login page if there is no user session"
              (let [client-details (cl-client/register-client "ClientTestApp" "localhost:3001") ;FORMAT => {:client-secret "XLFCQRKJXSV3T6YQJL5ZJJVGFUJNT6LD", :client-id "RXBX6ZXAER5KPDSZ3ZCZJDOBS27FLDE7", :name "ClientTestApp", :url "localhost:3001"}
-                   request (-> (r/request :get "/authorisation" {:client_id (:client-id client-details) :response_type "code" :redirect_uri "callback"})
-                               (assoc :params {:client_id (:client-id client-details) :response_type "code" :redirect_uri "callback"})
+                   request (-> (create-request :get (routes/path :authorise)
+                                               {:client_id (:client-id client-details)
+                                                :response_type "code"
+                                                :redirect_uri "callback"})
                                (r/header "accept" "text/html"))
                    response (oauth/authorise request)]
                (:status response) => 302
@@ -40,8 +42,10 @@
              (let [user (user/store-user! user-email "password")
                    client-details (cl-client/register-client "MYAPP" "myapp.com") ; NB this saves into the client store
                    access-token (cl-token/create-token client-details user) ; NB this saves into the token store
-                   request (-> (r/request :get "/authorisation")
-                               (assoc :params {:client_id (:client-id client-details) :response_type "code" :redirect_uri "callback"})
+                   request (-> (create-request :get (routes/path :authorise)
+                                               {:client_id (:client-id client-details)
+                                                :response_type "code"
+                                                :redirect_uri "callback"})
                                (r/header "accept" "text/html")
                                (assoc-in [:session :access_token] (:token access-token))
                                (assoc-in [:session :user-login] (:login user))
@@ -60,8 +64,9 @@
                    client-details (cl-client/register-client "MYAPP" "myapp.com")
                    access-token (cl-token/create-token client-details user)
                    csrf-token "CSRF-TOKEN"
-                   request (-> (r/request :post "/authorisation")
-                               (assoc :params {:client_id (:client-id client-details) :response_type "code" :redirect_uri "callback" :csrf-token csrf-token})
+                   request (-> (create-request :post (routes/path :authorise)
+                                               {:client_id (:client-id client-details) :response_type "code"
+                                                :redirect_uri "callback" :csrf-token csrf-token})
                                (assoc-in [:session :access_token] (:token access-token))
                                (assoc-in [:session :csrf-token] csrf-token)
                                (assoc :content-type "application/x-www-form-urlencoded") ;To mock a form post
@@ -76,8 +81,9 @@
                    client-details (cl-client/register-client "MYAPP" "myapp.com")
                    updated-user (user/add-authorised-client-for-user! user-email (:client-id client-details))
                    access-token (cl-token/create-token client-details user)
-                   request (-> (r/request :get "/authorisation")
-                               (assoc :params {:client_id (:client-id client-details) :response_type "code" :redirect_uri "callback"})
+                   request (-> (create-request :get (routes/path :authorise)
+                                               {:client_id (:client-id client-details) :response_type "code"
+                                                :redirect_uri "callback"})
                                (assoc-in [:session :access_token] (:token access-token))
                                (assoc-in [:session :user-login] (:login user)))
                    response (oauth/authorise request)]
@@ -88,8 +94,9 @@
              (let [user (user/store-user! user-email "password")
                    client-details (cl-client/register-client "MYAPP" "myapp.com")
                    access-token (cl-token/create-token client-details user)
-                   request (-> (r/request :post "/authorisation")
-                               (assoc :params {:client_id (:client-id client-details) :response_type "code" :redirect_uri "callback"})
+                   request (-> (create-request :post (routes/path :authorise) nil)
+                               (assoc :params {:client_id (:client-id client-details)
+                                               :response_type "code" :redirect_uri "callback"})
                                (assoc-in [:session :access_token] (:token access-token))
                                (assoc-in [:session :user-login] (:login user))
                                ;; stale csrf token can cause session to be lost
@@ -109,7 +116,7 @@
                    html/html-snippet
                    (html/select [:.clj--app-name])) => (has some element-has-correct-client-name-fn)
                (provided (client/retrieve-client "CLIENT_ID") => {:client-id "CLIENT_ID" :name "CLIENT_NAME"})))
-       
+
        (fact "redirects to error 404 page if client_id doesn't match a registered client"
                (-> (create-request :get (routes/path :show-authorise-form) {:client_id "CLIENT_ID"})
                    oauth/show-authorise-form) => (contains {:status 404})
@@ -117,14 +124,14 @@
 
 (facts "about show-authorise-failure"
        (fact "when authorisation failure is rendered will add error=access_denied in the querystring of the callback uri"
-             (let [request (-> (r/request :get "/authorise-failure")
+             (let [request (-> (create-request :get "/authorise-failure" nil)
                                (assoc-in [:context :translator] {})
                                (assoc-in [:session :redirect-uri] "http://where.do.we.go.now"))
                    response (oauth/show-authorise-failure request)]
                (:status response)) => 200
              (provided
                (oauth/add-error-to-uri "http://where.do.we.go.now") => anything))
-       
+
        (fact "authorisation failure page is rendered with client name"
              (let [element-has-correct-client-name-fn (fn [element] (= (html/text element) "CLIENT_NAME"))]
                (-> (create-request :get (routes/path :show-authorise-failure) nil)
@@ -156,10 +163,9 @@
        (let [user (user/store-user! user-email "password")
              client-details (cl-client/register-client "MYAPP" "myapp.com")
              auth-code (cl-auth-code/create-auth-code client-details user "callback")
-             request (-> (r/request :get "/token")
-                         (assoc :params {:grant_type   "authorization_code"
-                                         :redirect_uri "callback"
-                                         :code         (:code auth-code)})
+             request (-> (create-request :get (routes/path :validate-token) {:grant_type   "authorization_code"
+                                                                             :redirect_uri "callback"
+                                                                             :code         (:code auth-code)})
                          (create-auth-header client-details))
              response (oauth/validate-token request)
              response-body (-> response :body (json/parse-string keyword))]
@@ -181,16 +187,14 @@
 
 (facts "about auto-approver"
        (fact "returns true if user has authorised the client"
-             (-> (r/request :get "/authorisation")
-                 (assoc :params {:client_id ...client-id...})
+             (-> (create-request :get (routes/path :authorise) {:client_id ...client-id...})
                  (assoc-in [:session :user-login] ...email...)
                  oauth/auto-approver) => true
              (provided
                (user/is-authorised-client-for-user? ...email... ...client-id...) => true))
 
        (fact "returns false if user has not authorised the client"
-             (-> (r/request :get "/authorisation")
-                 (assoc :params {:client_id ...client-id...})
+             (-> (create-request :get (routes/path :authorise) {:client_id ...client-id...})
                  (assoc-in [:session :user-login] ...email...)
                  oauth/auto-approver) => false
              (provided
