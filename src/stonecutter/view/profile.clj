367f1fc87d1f2167135b6cfd5fa85abefb362355
Duncan Mortimer <dmortime@thoughtworks.com>
2015-07-17 13:32:03 +0100

    DM + RS | 6 | Update clj class for app-name in authorisedAppItem mixin

--- src/stonecutter/view/profile.clj
+++ src/stonecutter/view/profile.clj
@@ -19,7 +19,7 @@
   (html/at application-list-item
            [:.clj--authorised-app__list-item]
            (html/clone-for [client authorised-clients]
-                           [:.clj--authorised-app__title] (html/content (:name client))
+                           [:.clj--app-name] (html/content (:name client))
                            [:.clj--app-item__unshare-link] (html/set-attr :href (str (r/path :show-unshare-profile-card)
                                                                                      "?client_id="
                                                                                      (:client-id client))))))
