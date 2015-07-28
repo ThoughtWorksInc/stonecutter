b9a88d815510607f5d7b1d35d31bdb04faf1ebf3
Rob Speller <rspeller@thoughtworks.com>
2015-07-28 17:32:09 +0100

    AB & RS | 7 | Add unit test for enlive response to check it injects app name

--- test/stonecutter/test/helper.clj
+++ test/stonecutter/test/helper.clj
@@ -20,9 +20,17 @@
 (fact "If context has an alternative theme, then enlive-response swaps in that one"
       (let [html "<html><head><link data-clojure-id=\"theme-link\" href=\"stylesheets/application.css\" /></head></html>"
             enlive-m (html/html-snippet html)]
-        (fact "there is no theme in session"
+        (fact "there is no theme in context"
               (-> (helper/enlive-response enlive-m {}) get-response-enlive-m get-link-href)
                   => "stylesheets/application.css")
-        (fact "theme called dcent in session modifiers the header to point to dcent_theme.css"
+        (fact "theme called dcent in context modifiers the header to point to dcent_theme.css"
               (-> (helper/enlive-response enlive-m {:theme {:theme "dcent"}}) get-response-enlive-m get-link-href)
                   => "stylesheets/dcent_theme.css")))
+
+(fact "Enlive response injects the app name anywhere where class is clj--app-name"
+      (let [html "<html><body><h1 class=\"clj--app-name\"></h1><span class=\"clj--app-name\"></span></body></html>"
+            enlive-m (html/html-snippet html)]
+        (-> (helper/enlive-response enlive-m {:theme {:app-name "My App"}}) get-response-enlive-m
+            (html/select [:h1]) first html/text) => "My App"
+        (-> (helper/enlive-response enlive-m {:theme {:app-name "My App"}}) get-response-enlive-m
+            (html/select [:span]) first html/text) => "My App"))
