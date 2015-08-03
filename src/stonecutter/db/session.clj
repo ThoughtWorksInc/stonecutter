42334376c028ca4cfe12be5e2e438d5eb7181cf3
John Cowie <jcowie@thoughtworks.com>
2015-08-03 13:41:01 +0100

    JC | Replaced mongo session store with existing monger implementation

--- src/stonecutter/db/session.clj
+++ src/stonecutter/db/session.clj
@@ -1,23 +1,9 @@
 (ns stonecutter.db.session
-  (:require [ring.middleware.session.store :as store]
-            [monger.collection :as monger-c]
-            [stonecutter.util.uuid :as uuid]))
+  (:require [monger.ring.session-store :as mongo-session]))
 
 (def collection "session")
 
-(defrecord MongoSessionStore [mongo-db]
-  store/SessionStore
-  (read-session [_ key]
-    (->
-      (monger-c/find-map-by-id mongo-db collection key)
-      (dissoc :_id)))
-  (write-session [_ key data]
-    (let [k (or key (uuid/uuid))]
-      (monger-c/insert mongo-db collection (assoc data :_id k))))
-  (delete-session [_ key]
-    (monger-c/remove-by-id mongo-db collection key)))
-
 (defn mongo-session-store [mongo-db]
-  (MongoSessionStore. mongo-db))
+  (mongo-session/session-store mongo-db collection))
 
 
