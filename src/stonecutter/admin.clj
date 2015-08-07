1abfb215d74b35122042e46ad7e1649c9a6a4116
Christoph Wong <chwong@thoughtworks.com>
2015-08-07 11:54:18 +0100

    RP + CW | 44 | pretty logging for admin registration at startup

--- src/stonecutter/admin.clj
+++ src/stonecutter/admin.clj
@@ -1,6 +1,7 @@
 (ns stonecutter.admin
   (:require [stonecutter.config :as config]
             [stonecutter.validation :as v]
+            [clojure.tools.logging :as log]
             [stonecutter.db.user :as u]))
 
 (defn create-admin-user [config-m user-store]
@@ -10,8 +11,8 @@
         errors (v/validate-registration-email admin-login duplication-checker)]
     (when (and admin-login admin-password)
       (case errors
-        :duplicate nil
-        :invalid (throw Exception "exceptional")
+        :duplicate (log/info "Admin account already exists.")
+        :invalid (throw (Exception. "INVALID ADMIN DETAILS - please check that admin login is in the correct format"))
         nil (u/store-admin!
               user-store
               admin-login
