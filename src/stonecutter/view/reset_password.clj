6387e3bd96c1503991964890d50e79b7b4360ed2
John Cowie <jcowie@thoughtworks.com>
2015-08-10 18:11:35 +0100

    JC | #27 | Reused error rendering from change password for reset password

--- src/stonecutter/view/reset_password.clj
+++ src/stonecutter/view/reset_password.clj
@@ -1,13 +1,17 @@
 (ns stonecutter.view.reset-password
   (:require [net.cgrand.enlive-html :as html]
             [stonecutter.routes :as r]
-            [stonecutter.view.view-helpers :as vh]))
+            [stonecutter.view.view-helpers :as vh]
+            [stonecutter.view.change-password :as cp]))
 
 (defn set-form-action [enlive-m forgotten-password-id]
   (html/at enlive-m [:form] (html/set-attr :action (r/path :reset-password :forgotten-password-id forgotten-password-id))))
 
 (defn reset-password [request]
-  (let [forgotten-password-id (get-in request [:params :forgotten-password-id] "no-id-provided")]
+  (let [err (get-in request [:context :errors])
+        forgotten-password-id (get-in request [:params :forgotten-password-id] "no-id-provided")]
     (->
      (vh/load-template "public/reset-password.html")
-     (set-form-action forgotten-password-id))))
+     (set-form-action forgotten-password-id)
+     vh/add-anti-forgery
+     (cp/add-errors err))))
--- test/stonecutter/test/view/reset_password.clj
+++ test/stonecutter/test/view/reset_password.clj
@@ -2,7 +2,8 @@
   (:require [midje.sweet :refer :all]
             [stonecutter.routes :as r]
             [stonecutter.view.reset-password :as rp]
-            [stonecutter.test.view.test-helpers :as th]))
+            [stonecutter.test.view.test-helpers :as th]
+            [net.cgrand.enlive-html :as html]))
 
 (fact
  (th/test-translations "Reset password" rp/reset-password))
@@ -12,11 +13,26 @@
         page => (th/has-form-action? (r/path :reset-password :forgotten-password-id "some-uuid"))
         page => (th/has-form-method? "post")))
 
-(facts "about validation messages"
+(facts "about displaying errors"
        (tabular
-        (let [page (-> (th/create-request {} ?errors) rp/reset-password)]
-          (fact "validation-summary--show class is added to the validation summary element")
-          (fact "validation message is present as a validation summary item")
-          (fact "correct error messages are displayed"))
-        ?errors   ?validation-translations           ?highlighted-elements
-        {}))
+         (let [page (-> (th/create-request {} ?errors {:forgotten-password-id "some-uuid"}) rp/reset-password)]
+           (fact "validation-summary--show class is added to the validation summary element"
+                 (-> (html/select page [[:.clj--validation-summary :.validation-summary--show]])) =not=> empty?)
+           (fact "validation message is present as a validation summary item"
+                 (html/select page [:.clj--validation-summary__item]) =not=> empty?)
+           (fact "correct error messages are displayed"
+                 (->> (html/select page [:.clj--validation-summary__item])
+                      (map #(get-in % [:attrs :data-l8n]))) => ?validation-translations)
+           (fact "correct elements are highlighted"
+                 (->> (html/select page [:.form-row--validation-error])
+                      (map #(get-in % [:attrs :class]))) => (contains ?highlighted-elements)))
+
+         ?errors                          ?validation-translations                                                          ?highlighted-elements
+         {:new-password :blank}           ["content:change-password-form/new-password-blank-validation-message"]            [#"clj--new-password"]
+         {:new-password :too-short}       ["content:change-password-form/new-password-too-short-validation-message"]        [#"clj--new-password"]
+         {:new-password :too-long}        ["content:change-password-form/new-password-too-long-validation-message"]         [#"clj--new-password"]
+         {:confirm-new-password :invalid} ["content:change-password-form/confirm-new-password-invalid-validation-message"]  [#"clj--confirm-new-password"]
+         {:new-password :too-short
+          :confirm-new-password :invalid} ["content:change-password-form/new-password-too-short-validation-message"
+                                           "content:change-password-form/confirm-new-password-invalid-validation-message"]  [#"clj--new-password"
+                                                                                                                             #"clj--confirm-new-password"]))
