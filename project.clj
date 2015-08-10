bb2dc89a9dcfe6b45100f53a534a281cdb085c98
John Cowie <jcowie@thoughtworks.com>
2015-08-10 16:28:38 +0100

    JC | #27 | Added translations for reset password page

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                            [hiccup "1.0.5"]
                            [com.cemerick/url "0.1.1"]
                            [johncowie/clauth "2.0.1"]
-                           [traduki "0.1.1-SNAPSHOT"]
+                           [traduki "0.1.2-SNAPSHOT"]
                            [clj-yaml "0.4.0"]
                            [environ "1.0.0"]
                            [com.novemberain/monger "2.1.0"]
