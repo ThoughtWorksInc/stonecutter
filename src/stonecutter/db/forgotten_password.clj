44755ba4a071e28c08da23658a7b07d81be6f3ff
John Cowie <jcowie@thoughtworks.com>
2015-08-11 16:12:57 +0100

    JC | #27 | Added function for retrieving forgotten password record by user id

--- src/stonecutter/db/forgotten_password.clj
+++ src/stonecutter/db/forgotten_password.clj
@@ -1,6 +1,14 @@
 (ns stonecutter.db.forgotten-password
-  (:require [clauth.store :as cl-store]))
+  (:require [clauth.store :as cl-store]
+            [stonecutter.db.mongo :as m]))
 
 (defn store-id-for-user! [forgotten-password-store forgotten-password-id login]
   (let [doc {:forgotten-password-id forgotten-password-id :login login}]
     (cl-store/store! forgotten-password-store :forgotten-password-id doc)))
+
+(defn forgotten-password-doc-by-login [forgotten-password-store login]
+  (let [docs (m/query forgotten-password-store {:login login})]
+    (case (count docs)
+      0 nil
+      1 (first docs)
+      (throw (Exception. (format "Multiple forgotten password records found for login [%s]" login))))))
--- test/stonecutter/test/db/forgotten_password.clj
+++ test/stonecutter/test/db/forgotten_password.clj
@@ -10,4 +10,23 @@
              login "bob@burgers.com"]
          (fp/store-id-for-user! forgotten-password-store forgotten-password-id login)
          (cl-store/entries forgotten-password-store) => [{:forgotten-password-id forgotten-password-id
-                                                          :login login}]))
+                                                          :login                 login}]))
+
+(facts "about fetching forgotten password doc by id"
+       (fact "can fetch single doc by username"
+             (let [forgotten-password-store (m/create-memory-store)
+                   forgotten-password-id "asdf"
+                   login "bob@burgers.com"]
+               (fp/store-id-for-user! forgotten-password-store forgotten-password-id login)
+               (fp/forgotten-password-doc-by-login forgotten-password-store login) => {:forgotten-password-id forgotten-password-id
+                                                                                    :login                 login}
+               ))
+       (fact "if there are no matching docs then nil is returned"
+             (let [forgotten-password-store (m/create-memory-store)]
+               (fp/forgotten-password-doc-by-login forgotten-password-store "a@a.com") => nil))
+       (fact "if there are multiple matching docs then exception is thrown"
+             (let [forgotten-password-store (m/create-memory-store)
+                   login "bob@burgers.com"]
+               (fp/store-id-for-user! forgotten-password-store "id1" login)
+               (fp/store-id-for-user! forgotten-password-store "id2" login)
+               (fp/forgotten-password-doc-by-login forgotten-password-store login) => (throws Exception))))
