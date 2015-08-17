38cce5e1369f50ee493f2fbe6599fd6e22fbd405
John Cowie <jcowie@thoughtworks.com>
2015-08-17 15:27:23 +0100

    JC | Added slf4j dependency to stop logging breaking

--- project.clj
+++ project.clj
@@ -21,8 +21,10 @@
                            [ragtime "0.4.2"]
                            [garden "1.2.5"]
                            [clj-time "0.10.0"]
-                           [org.bitbucket.b_c/jose4j "0.4.4"]]
+                           [org.bitbucket.b_c/jose4j "0.4.4"]
+                           [org.slf4j/slf4j-simple "1.7.12"]]
             :main stonecutter.handler
+            :jvm-opts ["-Dlog4j.configuration=log4j.dev"]
             :aot :all
             :profiles {:dev {:dependencies   [[ring-mock "0.1.5"]
                                               [midje "1.7.0"]
@@ -37,7 +39,6 @@
                                     :handler stonecutter.lein/lein-app
                                     :init    stonecutter.lein/lein-ring-init
                                     :stacktrace-middleware prone.middleware/wrap-exceptions}
-                             :jvm-opts ["-Dlog4j.configuration=log4j.dev"]
                              :resource-paths ["resources" "test-resources"]
                              :aliases        {"test"        ["do" "clean," "midje"]
                                               "unit"        ["midje" "stonecutter.test.*"]
