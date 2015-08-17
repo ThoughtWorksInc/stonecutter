14694e0f7b1153262999673e0f03b798015c7651
Rob Speller <rspeller@thoughtworks.com>
2015-08-17 12:23:46 +0100

    DM + RS | 153 | Add test to check that token expiry time is set correctly

--- test/stonecutter/test/jwt.clj
+++ test/stonecutter/test/jwt.clj
@@ -19,16 +19,26 @@
     {:sub (.getSubject claims-set)
      :iss (.getIssuer claims-set)
      :aud (.getAudience claims-set)
+     :iat (-> claims-set .getIssuedAt .getValue)
+     :exp (-> claims-set .getExpirationTime .getValue)
      :email (.getClaimValue claims-set "email")}))
 
-(fact "id tokens can be generated and signed"
-      (let [rsa-key-pair (doto (RsaJwkGenerator/generateJwk 2048)
-                           (.setKeyId  "k1"))
-            id-token-generator (jwt/create-generator rsa-key-pair)
-            id-token (id-token-generator iss sub aud token-lifetime-minutes email)]
-        (:sub (decode rsa-key-pair aud id-token)) => sub
-        (:iss (decode rsa-key-pair aud id-token)) => iss
-        (:aud (decode rsa-key-pair aud id-token)) => [aud]
-        (:email (decode rsa-key-pair aud id-token)) => email))
+(facts "about generating id tokens"
+       (let [rsa-key-pair (doto (RsaJwkGenerator/generateJwk 2048)
+                            (.setKeyId  "k1"))
+             id-token-generator (jwt/create-generator rsa-key-pair)
+             id-token (id-token-generator iss sub aud token-lifetime-minutes email)]
+         (fact "id tokens can be generated and signed"
+               (:sub (decode rsa-key-pair aud id-token)) => sub
+               (:iss (decode rsa-key-pair aud id-token)) => iss
+               (:aud (decode rsa-key-pair aud id-token)) => [aud]
+               (:email (decode rsa-key-pair aud id-token)) => email)
+
+         (fact "token expiry time is set correctly, based on token lifetime in minutes"
+               (let [decoded-token (decode rsa-key-pair aud id-token)
+                     issued-at (:iat decoded-token)
+                     expiry (:exp decoded-token)
+                     token-lifetime-in-seconds (* 60 token-lifetime-minutes)]
+                 (- expiry issued-at) => token-lifetime-in-seconds))))
 
 
