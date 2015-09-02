563d9982b1995344fbde4253fde0bd9bd08f174a
Mark Levi <mdayanlevi@gmail.com>
2015-09-02 14:24:01 +0100

    RS & ML | 161 | Add all registration errors to index view and differentiate correctly between sign-in and registration validations

--- src/stonecutter/view/index.clj
+++ src/stonecutter/view/index.clj
@@ -8,12 +8,19 @@
   (html/at enlive-m field-row-selector (html/add-class "form-row--validation-error")))
 
 (def error-translations
-  {:sign-in-email       {:invalid  "content:index/sign-in-email-address-invalid-validation-message"
-                         :too-long "content:index/sign-in-email-address-too-long-validation-message"}
-   :sign-in-password    {:blank     "content:index/sign-in-password-blank-validation-message"
-                         :too-long  "content:index/sign-in-password-too-long-validation-message"
-                         :too-short "content:index/sign-in-password-too-short-validation-message"}
-   :sign-in-credentials {:invalid "content:index/sign-in-invalid-credentials-validation-message"}})
+  {:sign-in-email                 {:invalid  "content:index/sign-in-email-address-invalid-validation-message"
+                                   :too-long "content:index/sign-in-email-address-too-long-validation-message"}
+   :sign-in-password              {:blank     "content:index/sign-in-password-blank-validation-message"
+                                   :too-long  "content:index/sign-in-password-too-long-validation-message"
+                                   :too-short "content:index/sign-in-password-too-short-validation-message"}
+   :sign-in-credentials           {:invalid "content:index/sign-in-invalid-credentials-validation-message"}
+   :registration-email            {:invalid   "content:index/register-email-address-invalid-validation-message"
+                                   :duplicate "content:index/register-email-address-duplicate-validation-message"
+                                   :too-long  "content:index/register-email-address-too-long-validation-message"}
+   :registration-password         {:blank     "content:index/register-password-blank-validation-message"
+                                   :too-long  "content:index/register-password-too-long-validation-message"
+                                   :too-short "content:index/register-password-too-short-validation-message"}
+   :registration-confirm-password {:invalid "content:index/register-confirm-password-invalid-validation-message"}})
 
 (defn add-sign-in-email-error [err enlive-m]
   (if-let [sign-in-email-error (:sign-in-email err)]
@@ -35,9 +42,9 @@
   (if-let [sign-in-credentials-error (:sign-in-credentials err)]
     (let [error-translation (get-in error-translations [:sign-in-credentials sign-in-credentials-error])]
       (-> enlive-m
-          (html/at [:.clj--validation-summary] (html/add-class "validation-summary--show"))
-          (html/at [:.clj--validation-summary__item] (html/set-attr :data-l8n (or error-translation "content:index/sign-in-unknown-error")))))
-    (vh/remove-element enlive-m [:.clj--validation-summary])))
+          (html/at [:.clj--sign-in-validation-summary] (html/add-class "validation-summary--show"))
+          (html/at [:.clj--sign-in-validation-summary__item] (html/set-attr :data-l8n (or error-translation "content:index/sign-in-unknown-error")))))
+    (vh/remove-element enlive-m [:.clj--sign-in-validation-summary])))
 
 (defn add-sign-in-errors [err enlive-m]
   (->> enlive-m
@@ -45,34 +52,38 @@
        (add-sign-in-password-error err)
        (add-invalid-credentials-error err)))
 
-(defn set-email-input [params enlive-m]
+(defn set-sign-in-email-input [params enlive-m]
   (html/at enlive-m
-           [:.clj--email__input] (html/set-attr :value (:sign-in-email params))))
+           [:.clj--sign-in-email__input] (html/set-attr :value (:sign-in-email params))))
+
+(defn set-registration-email-input [params enlive-m]
+  (html/at enlive-m
+           [:.clj--registration-email__input] (html/set-attr :value (:registration-email params))))
 
 
 (defn add-registration-email-error [enlive-m err]
-  (if (contains? err :email)
-    (let [error-translation (get-in error-translations [:email (:email err)])]
+  (if-let [registration-email-error (:registration-email err)]
+    (let [error-translation (get-in error-translations [:registration-email registration-email-error])]
       (-> enlive-m
           (add-error-class [:.clj--registration-email])
-          (html/at [:.clj--registration-email__validation] (html/set-attr :data-l8n (or error-translation "content:registration-form/unknown-error")))))
+          (html/at [:.clj--registration-email__validation] (html/set-attr :data-l8n (or error-translation "content:index/register-unknown-error")))))
     (vh/remove-element enlive-m [:.clj--registration-email__validation])))
 
 (defn add-registration-password-error [enlive-m err]
-  (if (contains? err :password)
-    (let [error-translation (get-in error-translations [:password (:password err)])]
+  (if-let [registration-password-error (:registration-password err)]
+    (let [error-translation (get-in error-translations [:registration-password registration-password-error])]
       (-> enlive-m
           (add-error-class [:.clj--registration-password])
-          (html/at [:.clj--registration-password__validation] (html/set-attr :data-l8n (or error-translation "content:registration-form/unknown-error")))))
+          (html/at [:.clj--registration-password__validation] (html/set-attr :data-l8n (or error-translation "content:index/register-unknown-error")))))
     (vh/remove-element enlive-m [:.clj--registration-password__validation])))
 
 (defn add-registration-confirm-password-error [enlive-m err]
-  (if (contains? err :confirm-password)
-    (let [error-translation (get-in error-translations [:confirm-password (:confirm-password err)])]
+  (if-let [registration-confirm-password-error (:registration-confirm-password err)]
+    (let [error-translation (get-in error-translations [:registration-confirm-password registration-confirm-password-error])]
       (-> enlive-m
-          (html/at [:.clj--validation-summary] (html/add-class "validation-summary--show"))
-          (html/at [:.clj--validation-summary__item] (html/set-attr :data-l8n (or error-translation "content:registration-form/unknown-error")))))
-    (vh/remove-element enlive-m [:.clj--validation-summary])))
+          (html/at [:.clj--registration-validation-summary] (html/add-class "validation-summary--show"))
+          (html/at [:.clj--registration-validation-summary__item] (html/set-attr :data-l8n (or error-translation "content:index/register-unknown-error")))))
+    (vh/remove-element enlive-m [:.clj--registration-validation-summary])))
 
 (defn add-registration-errors [err enlive-m]
   (-> enlive-m
@@ -88,6 +99,7 @@
          (vh/set-attribute [:.clj--forgot-password] :href (r/path :show-forgotten-password-form))
          (add-sign-in-errors error-m)
          (add-registration-errors error-m)
-         (set-email-input (:params request))
+         (set-sign-in-email-input (:params request))
+         (set-registration-email-input (:params request))
          vh/remove-work-in-progress)))
 
--- test/stonecutter/test/view/index.clj
+++ test/stonecutter/test/view/index.clj
@@ -37,7 +37,7 @@
          (fact "password validation element is removed"
                page => (th/element-absent? [:.clj--sign-in-password__validation]))
          (fact "validation summary element is removed"
-               page => (th/element-absent? [:.clj--validation-summary]))))
+               page => (th/element-absent? [:.clj--sign-in-validation-summary]))))
 
 (facts "about displaying sign-in errors"
        (facts "when email is invalid"
@@ -52,7 +52,7 @@
                       page => (th/has-attr? [:.clj--sign-in-email__validation]
                                             :data-l8n "content:index/sign-in-email-address-invalid-validation-message"))
                 (fact "invalid value is preserved in input field"
-                      page => (th/has-attr? [:.clj--email__input] :value "invalid"))))
+                      page => (th/has-attr? [:.clj--sign-in-email__input] :value "invalid"))))
 
        (facts "when email is too long"
               (let [long-email-address (apply str (repeat 255 "x"))
@@ -119,4 +119,90 @@
          (fact "password validation element is removed"
                (html/select page [:.clj--registration-password__validation]) => empty?)
          (fact "validation summary element is removed"
-               (html/select page [:.clj--validation-summary]) => empty?)))
+               (html/select page [:.clj--registration-validation-summary]) => empty?)))
+
+(facts "about displaying registration errors"
+       (facts "when email is invalid"
+              (let [errors {:registration-email :invalid}
+                    params {:registration-email "invalidEmail"}
+                    page (-> (th/create-request {} errors params) index)]
+                (fact "the class for styling errors is added"
+                      (html/select page [[:.clj--registration-email :.form-row--validation-error]]) =not=> empty?)
+                (fact "email validation element is present"
+                      (html/select page [:.clj--registration-email__validation]) =not=> empty?)
+                (fact "correct error message is displayed"
+                      (html/select page [[:.clj--registration-email__validation (html/attr= :data-l8n "content:index/register-email-address-invalid-validation-message")]]) =not=> empty?)
+                (fact "invalid value is preserved in input field"
+                      (-> page (html/select [:.clj--registration-email__input]) first :attrs :value) => "invalidEmail")))
+
+       (facts "when email is a duplicate"
+              (let [errors {:registration-email :duplicate}
+                    params {:registration-email "valid@email.com"}
+                    page (-> (th/create-request {} errors params) index)]
+                (fact "the class for styling errors is added"
+                      (html/select page [[:.clj--registration-email :.form-row--validation-error]]) =not=> empty?)
+                (fact "email validation element is present"
+                      (html/select page [:.clj--registration-email__validation]) =not=> empty?)
+                (fact "correct error message is displayed"
+                      (html/select page [[:.clj--registration-email__validation (html/attr= :data-l8n "content:index/register-email-address-duplicate-validation-message")]]) =not=> empty?)
+                (fact "duplicate email is preserved"
+                      (-> page (html/select [:.clj--registration-email__input]) first :attrs :value) => "valid@email.com")))
+
+       (facts "when email is too long"
+              (let [long-email-address (apply str (repeat 255 "x"))
+                    errors {:registration-email :too-long}
+                    params {:registration-email long-email-address}
+                    page (-> (th/create-request {} errors params) index)]
+                (fact "the class for styling errors is added"
+                      (html/select page [[:.clj--registration-email :.form-row--validation-error]]) =not=> empty?)
+                (fact "email validation element is present"
+                      (html/select page [:.clj--registration-email__validation]) =not=> empty?)
+                (fact "correct error message is displayed"
+                      (html/select page [[:.clj--registration-email__validation (html/attr= :data-l8n "content:index/register-email-address-too-long-validation-message")]]) =not=> empty?)))
+
+       (fact "when password is blank"
+             (let [errors {:registration-password :blank}
+                   params {:registration-password ""}
+                   page (-> (th/create-request {} errors params) index)]
+               (fact "the class for styling errors is added"
+                     (html/select page [[:.clj--registration-password :.form-row--validation-error]]) =not=> empty?)
+               (fact "password validation element is present"
+                     (html/select page [:.clj--registration-password__validation]) =not=> empty?)
+               (fact "correct error message is displayed"
+                     (html/select page [[:.clj--registration-password__validation (html/attr= :data-l8n "content:index/register-password-blank-validation-message")]]) =not=> empty?)))
+
+       (fact "when password is too short"
+             (let [errors {:registration-password :too-short}
+                   params {:registration-password "short"}
+                   page (-> (th/create-request {} errors params) index)]
+               (fact "the class for styling errors is added"
+                     (html/select page [[:.clj--registration-password :.form-row--validation-error]]) =not=> empty?)
+               (fact "password validation element is present"
+                     (html/select page [:.clj--registration-password__validation]) =not=> empty?)
+               (fact "correct error message is displayed"
+                     (html/select page [[:.clj--registration-password__validation (html/attr= :data-l8n "content:index/register-password-too-short-validation-message")]]) =not=> empty?)))
+
+       (fact "when password is too long"
+             (let [long-password (apply str (repeat 255 "x"))
+                   errors {:registration-password :too-long}
+                   params {:registration-password long-password}
+                   page (-> (th/create-request {} errors params) index)]
+               (fact "the class for styling errors is added"
+                     (html/select page [[:.clj--registration-password :.form-row--validation-error]]) =not=> empty?)
+               (fact "password validation element is present"
+                     (html/select page [:.clj--registration-password__validation]) =not=> empty?)
+               (fact "correct error message is displayed"
+                     (html/select page [[:.clj--registration-password__validation (html/attr= :data-l8n "content:index/register-password-too-long-validation-message")]]) =not=> empty?)))
+
+       (fact "when confirm password is invalid"
+             (let [errors {:registration-confirm-password :invalid}
+                   params {:registration-password "password"
+                           :registration-confirm-password "invalid-password"}
+                   page (-> (th/create-request {} errors params) index)]
+               (fact "validation-summary--show class is added to the validation summary element"
+                     (-> (html/select page [:.clj--registration-validation-summary])
+                         first :attrs :class) => (contains "validation-summary--show"))
+               (fact "confirm password validation is present as a validation summary item"
+                     (html/select page [:.clj--registration-validation-summary__item]) =not=> empty?)
+               (fact "correct error message is displayed"
+                     (html/select page [[:.clj--registration-validation-summary__item (html/attr= :data-l8n "content:index/register-confirm-password-invalid-validation-message")]]) =not=> empty?))))
