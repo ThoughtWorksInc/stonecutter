2ef2ea79de704e58d75e8f71954eb27eed5a8f72
Rob Speller <rspeller@thoughtworks.com>
2015-09-08 16:57:13 +0100

    RS & ML | 121 | removed error-404.jade; added translations to error-account-nonexistent page

--- src/stonecutter/view/error.clj
+++ src/stonecutter/view/error.clj
@@ -1,17 +1,21 @@
 (ns stonecutter.view.error
   (:require [net.cgrand.enlive-html :as html]
-            [stonecutter.view.view-helpers :as vh]))
+            [stonecutter.view.view-helpers :as vh]
+            [stonecutter.routes :as r]))
 
 (defn modify-error-translation-keys [enlive-map error-page-key]
   (html/at enlive-map
            [:body] (html/set-attr :class (str "func--" error-page-key "-page"))
            [:title] (html/set-attr :data-l8n (str "content:" error-page-key "/title"))
            [:.clj--error-page-header] (html/set-attr :data-l8n (str "content:" error-page-key "/page-header"))
-           [:.clj--error-page-intro] (html/set-attr :data-l8n (str "content:" error-page-key "/page-intro"))
-           [:.clj--error-page-content] (html/set-attr :data-l8n (str "html:" error-page-key "/page-content"))))
+           [:.clj--error-page-intro] (html/set-attr :data-l8n (str "content:" error-page-key "/page-intro"))))
+
+(defn set-return-to-link [enlive-m path]
+  (html/at enlive-m [:.clj--error-return-home__link] (html/set-attr :href path)))
 
 (defn internal-server-error []
-  (vh/load-template "public/error-500.html"))
+  (-> (vh/load-template "public/error-500.html")
+      (set-return-to-link (r/path :index))))
 
 (defn not-found-error []
   (-> (internal-server-error) (modify-error-translation-keys "error-404")))
@@ -23,4 +27,4 @@
   (-> (internal-server-error) (modify-error-translation-keys "error-forbidden")))
 
 (defn account-nonexistent []
-  (vh/load-template "public/error-account-nonexistent.html"))
+  (-> (internal-server-error) (modify-error-translation-keys "error-account-nonexistent")))
--- test/stonecutter/test/view/error.clj
+++ test/stonecutter/test/view/error.clj
@@ -1,6 +1,9 @@
 (ns stonecutter.test.view.error
   (:require [midje.sweet :refer :all]
             [net.cgrand.enlive-html :as html]
+            [stonecutter.helper :as helper]
+            [stonecutter.translation :as t]
+            [stonecutter.routes :as r]
             [stonecutter.test.view.test-helpers :as th]
             [stonecutter.view.error :as e]))
 
@@ -13,6 +16,19 @@
         modified-error-enlive-map => (th/has-attr? [:.clj--error-page-header]
                                                    :data-l8n "content:oops-error/page-header")
         modified-error-enlive-map => (th/has-attr? [:.clj--error-page-intro]
-                                                   :data-l8n "content:oops-error/page-intro")
-        modified-error-enlive-map => (th/has-attr? [:.clj--error-page-content]
-                                                   :data-l8n "html:oops-error/page-content")))
+                                                   :data-l8n "content:oops-error/page-intro")))
+
+(tabular
+  (facts "there are no missing translations"
+         (let [translator (t/translations-fn t/translation-map)
+               page (-> (?error-page-fn) (helper/enlive-response {:translator translator}) :body)]
+           page => th/no-untranslated-strings))
+  ?error-page-fn
+  e/account-nonexistent
+  e/csrf-error
+  e/forbidden-error
+  e/internal-server-error
+  e/not-found-error)
+
+(fact "home page link is set correctly"
+      (-> (e/not-found-error) (html/select [:.clj--error-return-home__link]) first :attrs :href) => (r/path :index))
