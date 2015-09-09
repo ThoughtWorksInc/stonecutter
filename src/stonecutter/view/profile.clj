1214c006c3d8c23f217ae60511a61ae2f34568d4
John Cowie <jcowie@thoughtworks.com>
2015-09-09 14:35:18 +0100

    JC | library html file loaded at run-time instead of compile time

--- src/stonecutter/view/profile.clj
+++ src/stonecutter/view/profile.clj
@@ -1,23 +1,22 @@
 (ns stonecutter.view.profile
-  (:require [traduki.core :as t]
-            [net.cgrand.enlive-html :as html]
+  (:require [net.cgrand.enlive-html :as html]
             [stonecutter.config :as config]
             [stonecutter.routes :as r]
             [stonecutter.view.view-helpers :as vh]))
 
-(defn add-username [request enlive-m]
+(defn add-username [enlive-m request]
   (let [email (get-in request [:session :user-login])]
     (html/at enlive-m
              [:.clj--card-email] (html/content email))))
 
 (def library-template (vh/load-template "public/library.html"))
 
-(def application-list-item (first (html/select library-template [:.clj--authorised-app__list-item])))
+(defn application-list-item [library-m] (first (html/select library-template [:.clj--authorised-app__list-item])))
 
-(def empty-application-list-item (first (html/select library-template [:.clj--authorised-app__list-item--empty])))
+(defn empty-application-list-item [library-m] (first (html/select library-template [:.clj--authorised-app__list-item--empty])))
 
-(defn application-list-items [authorised-clients]
-  (html/at application-list-item
+(defn application-list-items [authorised-clients library-m]
+  (html/at (application-list-item library-m)
            [:.clj--authorised-app__list-item]
            (html/clone-for [client authorised-clients]
                            [:.clj--client-name] (html/content (:name client))
@@ -25,11 +24,11 @@
                                                                                      "?client_id="
                                                                                      (:client-id client))))))
 
-(defn add-application-list [request enlive-m]
+(defn add-application-list [enlive-m request library-m]
   (let [authorised-clients (get-in request [:context :authorised-clients])]
     (if-not (empty? authorised-clients)
-      (html/at enlive-m [:.clj--app__list] (html/content (application-list-items authorised-clients)))
-      (html/at enlive-m [:.clj--app__list] (html/content empty-application-list-item)))))
+      (html/at enlive-m [:.clj--app__list] (html/content (application-list-items authorised-clients library-m)))
+      (html/at enlive-m [:.clj--app__list] (html/content (empty-application-list-item library-m))))))
 
 (defn set-sign-out-link [enlive-m]
   (html/at enlive-m
@@ -43,17 +42,17 @@
   (html/at enlive-m
            [:.clj--delete-account__link] (html/set-attr :href (r/path :show-delete-account-confirmation))))
 
-(defn hide-admin-span [request enlive-m]
+(defn hide-admin-span [enlive-m request]
   (let [role (get-in request [:context :role])]
     (html/at enlive-m [:.clj--admin__span]
              (when (= role (:admin config/roles)) identity))))
 
-(defn diplay-email-unconfirmed-message [request enlive-m]
+(defn diplay-email-unconfirmed-message [enlive-m request]
   (if (= false (get-in request [:context :confirmed?]))
     enlive-m
     (vh/remove-element enlive-m [:.clj--unconfirmed-email-message-container])))
 
-(defn set-flash-message [request enlive-m]
+(defn set-flash-message [enlive-m request]
   (case (:flash request)
     :password-changed (html/at enlive-m [:.clj--flash-message-text] (html/set-attr :data-l8n "content:flash/password-changed"))
     :email-confirmed (html/at enlive-m [:.clj--flash-message-text] (html/set-attr :data-l8n "content:flash/email-confirmed"))
@@ -61,13 +60,14 @@
     (vh/remove-element enlive-m [:.clj--flash-message-container])))
 
 (defn profile [request]
-  (->> (vh/load-template "public/profile.html")
-       (set-flash-message request)
-       (diplay-email-unconfirmed-message request)
-       (add-username request)
-       (add-application-list request)
-       set-sign-out-link
-       set-change-password-link
-       set-delete-account-link
-       (hide-admin-span request)
-       vh/remove-work-in-progress))
+  (let [library-m (vh/load-template "public/library.html")]
+    (->  (vh/load-template "public/profile.html")
+         (set-flash-message request)
+         (diplay-email-unconfirmed-message request)
+         (add-username request)
+         (add-application-list request library-m)
+         set-sign-out-link
+         set-change-password-link
+         set-delete-account-link
+         (hide-admin-span request)
+         vh/remove-work-in-progress)))
