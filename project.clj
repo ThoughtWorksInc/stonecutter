bf22f797289dd5ac299e3d43cb92cc88ebc829ae
John Cowie <jcowie@thoughtworks.com>
2015-09-09 15:58:08 +0100

    JC | Upgraded to scenic 0.2.5

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [ring/ring-defaults "0.1.5"]
                  [ring/ring-jetty-adapter "1.4.0"]
-                 [scenic "0.2.3"]
+                 [scenic "0.2.5"]
                  [enlive "1.1.6"]
                  [hiccup "1.0.5"]
                  [com.cemerick/url "0.1.1"]
