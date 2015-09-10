501c1db3aa2a622460af0910b1d11cd6c5a39bfe
Christoph Wong <chwong@thoughtworks.com>
2015-09-10 15:37:45 +0100

    AB + CW | 100 | more cljs-controllers logic for change password

--- src-cljs/stonecutter/controller/change_password.cljs
+++ src-cljs/stonecutter/controller/change_password.cljs
@@ -2,7 +2,33 @@
   (:require [stonecutter.validation :as v])
   (:require-macros [stonecutter.translation :as t]))
 
+;; CURRENT PASSWORD
 (defn update-current-password-blur [state]
-  (let [password (:current-password state)
+  (let [password (get-in state [:current-password :value])
         error (v/validate-password-format password)]
-    (assoc state :error error)))
+    (assoc-in state [:current-password :error] error)))
+
+(defn update-current-password-input [state]
+  (let [password (get-in state [:current-password :value])
+        error (v/validate-password-format password)]
+    (if-not error
+      (assoc-in state [:current-password :error] nil)
+      state)))
+
+;; NEW PASSWORD
+(defn update-new-password-blur [state]
+  (let [current-password (get-in state [:current-password :value])
+        new-password (get-in state [:new-password :value])
+        error (or (v/validate-password-format new-password)
+                  (v/validate-passwords-are-different current-password new-password))]
+    (assoc-in state [:new-password :error] error)))
+
+(defn update-new-password-input [state]
+  (let [password (get-in state [:new-password :value])
+        error (v/validate-password-format password)]
+    (if error
+      (assoc-in state [:new-password :tick] false)
+      (-> state
+        (assoc-in [:new-password :tick] true)
+        (assoc-in [:new-password :error] nil)))))
+
--- test-cljs/stonecutter/test/unit/change_password.cljs
+++ test-cljs/stonecutter/test/unit/change_password.cljs
@@ -6,11 +6,68 @@
 (deftest update-model-on-blur
          (testing "current-password"
                   (testing "when blank, field is invalid and error is blank"
-                           (let [current-state {:current-password "" :error nil}
-                                 expected-state {:current-password "" :error :blank}]
-                             (is (= expected-state (cp/update-current-password-blur current-state)))))
+                           (let [current-state {:current-password {:value "" :error nil} :new-password {}}
+                                 expected-state {:current-password {:value "" :error :blank} :new-password {}}]
+                             (is (= expected-state (cp/update-current-password-blur current-state))
+                                 "blank current password did not produce error on blur")))
 
                   (testing "when password is less than 8 characters, field is invalid and error is too-short"
-                           (let [current-state {:current-password "123" :error nil}
-                                 expected-state {:current-password "123" :error :too-short}]
-                             (is (= expected-state (cp/update-current-password-blur current-state)))))))
+                           (let [current-state {:current-password {:value "123" :error nil} :new-password {}}
+                                 expected-state {:current-password {:value "123" :error :too-short} :new-password {}}]
+                             (is (= expected-state (cp/update-current-password-blur current-state))
+                                 "short current password did not produce error on blur"))))
+
+         (testing "new-password"
+                  (testing "when blank, field is invalid and error is blank"
+                           (let [current-state {:current-password {} :new-password {:value "" :error nil}}
+                                 expected-state {:current-password {} :new-password {:value "" :error :blank}}]
+                             (is (= expected-state (cp/update-new-password-blur current-state))
+                                 "blank new password did not produce error on blur")))
+
+                  (testing "when password is less than 8 characters, field is invalid and error is too-short"
+                           (let [current-state {:current-password {} :new-password {:value "123" :error nil}}
+                                 expected-state {:current-password {} :new-password {:value "123" :error :too-short}}]
+                             (is (= expected-state (cp/update-new-password-blur current-state))
+                                 "short new password did not produce error on blur")))
+
+                  (testing "when password is same as current, field is invalid and error is unchanged"
+                           (let [current-state {:current-password {:value "12345678" :error nil} :new-password {:value "12345678" :error nil}}
+                                 expected-state {:current-password {:value "12345678" :error nil} :new-password {:value "12345678" :error :unchanged}}]
+                             (is (= expected-state (cp/update-new-password-blur current-state))
+                                 "unchanged new password did not produce error on blur")))))
+
+(deftest update-model-on-input
+         (testing "current-password"
+                  (testing "when input is valid, error should be nil"
+                           (let [current-state {:current-password {:value "12345678" :error :too-short} :new-password {}}
+                                 expected-state {:current-password {:value "12345678" :error nil} :new-password {}}]
+                             (is (= expected-state (cp/update-current-password-input current-state))
+                                 "correcting an error did not clear error in state")))
+
+                  (testing "when password is less than 8 characters, error is not changed"
+                           (let [current-state {:current-password {:value "123" :error :anything} :new-password {}}
+                                 expected-state {:current-password {:value "123" :error :anything} :new-password {}}]
+                             (is (= expected-state (cp/update-current-password-input current-state))
+                                 "short current password changed the error"))))
+
+         (testing "new-password"
+                  (testing "when input is valid, error should be nil, tickshould be true"
+                           (let [current-state {:current-password {} :new-password {:value "12345678" :error :something}}
+                                 expected-state {:current-password {} :new-password {:value "12345678" :error nil :tick true}}]
+                             (is (= expected-state (cp/update-new-password-input current-state))
+                                 "correcting an error did not clear error in state for new password")))
+
+                  (testing "when input is invalid, error should be the same, tick should be false"
+                           (let [current-state {:current-password {} :new-password {:value "1234567" :error :anything :tick true}}
+                                 expected-state {:current-password {} :new-password {:value "1234567" :error :anything :tick false}}]
+                             (is (= expected-state (cp/update-new-password-input current-state))
+                                 "short new password did not turn tick to false and/or changed the error")))
+                  ))
+
+;;NOTES
+; :new-password
+; {:value "12345678" :error nil} => add tick, everything is good
+; {:value "123456" :error nil} => no tick, no err msg
+; {:value "1234567" :error :too-short} => invalid field, add too-short err message
+; {:value "" :error :blank}
+; {:value "1234" :error :blank}
