cf50d2ca420fcb8cc4715a0260d47ecc7e8148fe
aewelch <aewelch@hotmail.co.uk>
2016-01-15 11:07:35 +0000

    AW | 301 | transparency removed from images to prevent error when converting to input stream

--- src/stonecutter/util/image.clj
+++ src/stonecutter/util/image.clj
@@ -9,7 +9,20 @@
   (:import (java.io ByteArrayInputStream ByteArrayOutputStream)
            (javax.imageio ImageIO)
            (org.apache.commons.io IOUtils)
-           (org.apache.commons.codec.binary Base64)))
+           (org.apache.commons.codec.binary Base64)
+           (java.awt.image BufferedImage)))
+
+(def image-height 150)
+(def image-width 150)
+
+(defn picture-data [picture]
+  (last (str/split picture #",")))
+
+(defn picture-type [picture]
+  (->> picture
+       (re-find #"/(\w{3,});")
+       last
+       str/upper-case))
 
 (defn check-file-type [request]
   (when (not (mt/image? (get-in request [:params :profile-photo :content-type])))
@@ -25,10 +38,17 @@
     (when (> file-size (config/image-upload-size-limit))
       :too-large)))
 
+(defn remove-transparency [transparent-image]
+  (let [opaque-image (BufferedImage. image-width image-height BufferedImage/TYPE_3BYTE_BGR)
+        input-rgb (.getRGB transparent-image 0 0 image-width image-height nil 0 image-width)]
+    (.setRGB opaque-image 0 0 image-width image-height input-rgb 0 image-width)
+    opaque-image))
+
 (defn buffered-image->input-stream [buffered-image content-type]
   (let [os (ByteArrayOutputStream.)
-        file-extension (mime/extension-for-name content-type)]
-    (ImageIO/write buffered-image (.substring file-extension 1) os)
+        file-extension (mime/extension-for-name content-type)
+        opaque-image (remove-transparency buffered-image)]
+    (ImageIO/write opaque-image (.substring file-extension 1) os)
     (ByteArrayInputStream. (.toByteArray os))))
 
 (defn resize-and-crop-image [file]
@@ -38,11 +58,11 @@
         height (last dimensions)]
     (if (< width height)
       (-> image
-          (resizer/resize-to-width 150)
-          (resizer/crop-to-height 150))
+          (resizer/resize-to-width image-width)
+          (resizer/crop-to-height image-height))
       (-> image
-          (resizer/resize-to-height 150)
-          (resizer/crop-to-width 150)))))
+          (resizer/resize-to-height image-height)
+          (resizer/crop-to-width image-width)))))
 
 (defn encode-base64 [file]
   (->> file
@@ -51,9 +71,3 @@
        Base64/encodeBase64
        (map char)
        (apply str "data:" (.getContentType file) ";base64,")))
-
-(defn picture-data [picture]
-  (last (str/split picture #",")))
-
-(defn picture-type [picture]
-  (str/upper-case (last (re-find #"/(\w{3,});" picture))))
