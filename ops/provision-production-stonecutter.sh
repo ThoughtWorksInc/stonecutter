3732811e3776e57e43141e61bd70f52892e6cc4e
aewelch <amyelwelch@gmail.com>
2016-05-11 15:06:04 +0100

    LF + AW | 403 | provisioning scripts defined for ansible deployment

---
+++ ops/provision-production-stonecutter.sh
@@ -0,0 +1,1 @@
+ansible-playbook dob_playbook.yml -i dob.inventory --extra-vars="CONFIG_FILE_PATH=stonecutter-production-config.env"
---
+++ ops/provision-staging-stonecutter.sh
@@ -0,0 +1,1 @@
+ansible-playbook dob_playbook.yml -i dob.inventory-staging --extra-vars="CONFIG_FILE_PATH=stonecutter-staging-config.env"
